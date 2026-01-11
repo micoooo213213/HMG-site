7d714f04c9a9978c64bd21df709f6287fd50b46f
micoooo213213 <mic88138@gmail.com>
2026-01-10 23:18:17 -0600

    Delete hmg-site directory

--- hmg-site/script.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector("nav");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 20) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-  }
-});
